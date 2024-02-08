4af7b317359cd42b72a85df3bb65c96de53d27de
codexuz <142141576+codexuz@users.noreply.github.com>
2024-02-08 09:43:08 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const OpenAI = require('openai');
+const cors = require('cors');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+const http = require("http");
+const server = http.createServer(app);
+const PORT = process.env.PORT;
+
+
+const openai = new OpenAI({
+  apiKey: process.env.Apikey
+});
+
+
+
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', async (req, res)=>{
+    res.send('Letter')
+})
+
+
+app.post('/report', async (req, res)=>{
+  const {transcript} = req.body
+
+   const completion = await openai.completions.create({
+    model: "gpt-3.5-turbo-instruct",
+    prompt: `Generate a high score IELTS essay sample to this topic with html <p> and <br> tags: ${body.question}
+    `,
+    max_tokens: 800,
+    temperature: 0,
+  });
+  
+   res.json(completion)
+})
+
+
+
+
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
